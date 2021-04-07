c730ca4fd422b184ca5966674a146acb4d1da4b6
Kittinan Samakpong <joker.use.3@gmail.com>
2021-04-07 11:27:50 +0700

    render ejs

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ app.use(express.static('public'));
 app.set('view engine','ejs');
 
 app.get('/',function(req,res){
-    res.send("Hello");
+    res.render("home.ejs");
 });
+
+app.get('/:hero',function(req,res){
+    let hero = req.params;
+    console.log(hero);
+})
 
 app.listen('3000',function(req,res){
     console.log('Server is running')
